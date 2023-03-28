7d0d93f0704c5881c354fa1eb732396824b50905
amanbhag <39967513+amanbhag@users.noreply.github.com>
2023-03-28 18:03:06 +0530

    updated import

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ import * as dotenv from "dotenv";
 import cors from "cors";
 import connectDB from "./mongodb/connect.js";
 import postRoutes from "./routes/postRoutes.js";
-import DalleRoutes from "./routes/DalleRoutes.js";
+import dalleRoutes from "./routes/dalleRoutes.js";
 
 dotenv.config();
 const app = express();
@@ -13,7 +13,7 @@ app.use(cors());
 app.use(express.json({ limit: "50mb" }));
 
 app.use("/api/v1/post", postRoutes);
-app.use("/api/v1/dalle", DalleRoutes);
+app.use("/api/v1/dalle", dalleRoutes);
 app.get("/", async (req, res) => {
   res.send("hello from DAll-e");
 });
